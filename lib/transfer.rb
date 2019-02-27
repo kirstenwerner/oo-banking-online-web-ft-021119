803fb0e1eb691c99c6453a9fc989191a73b4949c
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-27 07:02:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :reciever, :transfer_amount
+  
+  def initialize(sender, reciever, transfer_amount = 0)
+    @sender = sender
+    @reciever = reciever
+    @status = pending 
+    
+  
 end
